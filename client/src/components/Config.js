5e6f3d7d4e318b2345b698cdee6ec172f800315b
Noah <74864925+noah071610@users.noreply.github.com>
2021-05-30 09:36:42 +0900

    Update Config.js

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -5,4 +5,4 @@ export const API_URL = "https://api.themoviedb.org/3/";
 
 export const IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
 
-export const API_KEY = "71b64c8016237b0f86c9f0eba32f042a";
+export const API_KEY = process.env.API_KEY;
